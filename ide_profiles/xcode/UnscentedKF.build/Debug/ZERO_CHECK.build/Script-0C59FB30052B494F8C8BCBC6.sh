4a645013c97e9b509659ebb86d7b62438781645a
bt22dr <bt22dr@gmail.com>
2018-01-30 00:53:46 +0900

    프로젝트 정리

--- ide_profiles/xcode/UnscentedKF.build/Debug/ZERO_CHECK.build/Script-0C59FB30052B494F8C8BCBC6.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-make -C /Users/jy_song/Documents/Study/carnd/Term2/CarND-Unscented-Kalman-Filter-Project/ide_profiles/xcode -f /Users/jy_song/Documents/Study/carnd/Term2/CarND-Unscented-Kalman-Filter-Project/ide_profiles/xcode/CMakeScripts/ZERO_CHECK_cmakeRulesBuildPhase.make$CONFIGURATION all
